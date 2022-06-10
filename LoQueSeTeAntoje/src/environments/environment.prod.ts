f1100d7705e4ae984a2094f1931f57e586f29776
Matias Gravante <mgravante@navent.com>
2022-06-10 05:16:33 -0300

    algunos fixes de config

--- LoQueSeTeAntoje/src/environments/environment.prod.ts
+++ LoQueSeTeAntoje/src/environments/environment.prod.ts
@@ -8,6 +8,9 @@ export const environment = {
   appId: "1:474222220826:web:2fb951ba03b05aa204e3a3",
   measurementId: "G-W9CF2M9TY5"
   },
-  production:true
+  production:true,
+  fcmUrl:'https://fcm.googleapis.com/fcm/send',
+  fcmServerKey:'AAAAbmnY1ho:APA91bHz7Tjn1rchhUqoGeIxPK5fyYBjy3Nnnsnv2-9YrKq_lrJNoS5snVZctp1tkuJtL_IbURVPF8TpOrZ8Kngf5GKtXgymhPBeHqGAVtr8r6c7GbqvyukJGGJWmq49UyoCBgaa_FJv'
 }
+
 
--- LoQueSeTeAntoje/src/environments/environment.ts
+++ LoQueSeTeAntoje/src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
   firebaseConfig: {
-  apiKey: "AIzaSyBScvj9r8EBlCFHcyoh1V48CLfs9SK5JKo",
-  authDomain: "loqueseteantojeapp.firebaseapp.com",
-  projectId: "loqueseteantojeapp",
-  storageBucket: "loqueseteantojeapp.appspot.com",
-  messagingSenderId: "474222220826",
-  appId: "1:474222220826:web:2fb951ba03b05aa204e3a3",
-  measurementId: "G-W9CF2M9TY5"
+    apiKey: "AIzaSyBScvj9r8EBlCFHcyoh1V48CLfs9SK5JKo",
+    authDomain: "loqueseteantojeapp.firebaseapp.com",
+    projectId: "loqueseteantojeapp",
+    storageBucket: "loqueseteantojeapp.appspot.com",
+    messagingSenderId: "474222220826",
+    appId: "1:474222220826:web:2fb951ba03b05aa204e3a3",
+    measurementId: "G-W9CF2M9TY5"
   },
-  production:true,
-  fcmUrl:'https://fcm.googleapis.com/fcm/send',
-  fcmServerKey:'AAAAbmnY1ho:APA91bHz7Tjn1rchhUqoGeIxPK5fyYBjy3Nnnsnv2-9YrKq_lrJNoS5snVZctp1tkuJtL_IbURVPF8TpOrZ8Kngf5GKtXgymhPBeHqGAVtr8r6c7GbqvyukJGGJWmq49UyoCBgaa_FJv'
+  production: false,
+  fcmUrl: 'https://fcm.googleapis.com/fcm/send',
+  fcmServerKey: 'AAAAbmnY1ho:APA91bHz7Tjn1rchhUqoGeIxPK5fyYBjy3Nnnsnv2-9YrKq_lrJNoS5snVZctp1tkuJtL_IbURVPF8TpOrZ8Kngf5GKtXgymhPBeHqGAVtr8r6c7GbqvyukJGGJWmq49UyoCBgaa_FJv'
 }
 
 
